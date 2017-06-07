77afcda56c4851415bc1fe00797d52eecc7e3a17
Rafid Hoda <rafidhoda@gmail.com>
2017-06-07 19:05:09 +0300

    Add Visa page

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
 
   get 'rsvp', to: 'rsvps#new'
 
+  get 'visa' => 'pages#visa'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
