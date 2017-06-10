c904c61eadd14268312ca130f90cb5e7c48799c0
Rafid Hoda <rafidhoda@gmail.com>
2017-06-10 14:47:36 +0300

    Validate fields

--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -1,4 +1,9 @@
 class Rsvp < ApplicationRecord
   has_many :guests, dependent: :destroy
   accepts_nested_attributes_for :guests, allow_destroy: true, reject_if: :all_blank
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :phone_number, presence: true
 end
