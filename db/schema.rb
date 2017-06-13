c5f21ff7ec829022e561eb7d3704c16142a6c1f2
Rafid Hoda <rafidhoda@gmail.com>
2017-06-13 18:23:41 +0300

    Add Posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170613124829) do
+ActiveRecord::Schema.define(version: 20170613151654) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,14 @@ ActiveRecord::Schema.define(version: 20170613124829) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["rsvp_id"], name: "index_guests_on_rsvp_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.text "body"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "rsvps", force: :cascade do |t|
@@ -61,4 +69,5 @@ ActiveRecord::Schema.define(version: 20170613124829) do
   end
 
   add_foreign_key "guests", "rsvps"
+  add_foreign_key "posts", "users"
 end
